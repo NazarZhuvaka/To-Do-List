33c861e6cce3dcf799425098c67925cb945f7583
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-05-17 14:55:15 +0300

    commit

--- script.js
+++ script.js
@@ -4,37 +4,55 @@ const taskInput = document.querySelector('.task-input')
 const taskGroup = document.querySelector('.list-group')
 
 
-addtaskBtn.addEventListener('click' , function(){
-    if (taskInput.value === '') {
-        alert('You must write something!')
-    } else {
-        let newTask = document.createElement('li')
-        newTask.className = 'list-group-item'
-        newTask.innerHTML = taskInput.value;
+// addtaskBtn.addEventListener('click' , function(){
+//     if (taskInput.value === '') {
+//         alert('You must write something!')
+//     } else {
+//         let newTask = document.createElement('li')
+//         newTask.className = 'list-group-item'
+//         newTask.innerHTML = taskInput.value;
         
-        taskGroup.appendChild(newTask);
+//         taskGroup.appendChild(newTask);
 
-        let closeIcon = document.createElement('span')
-        closeIcon.innerHTML = "\u00d7";
-        closeIcon.className = 'task__hide'
-        newTask.appendChild(closeIcon)
+//         let closeIcon = document.createElement('span')
+//         closeIcon.innerHTML = "\u00d7";
+//         closeIcon.className = 'task__hide'
+//         newTask.appendChild(closeIcon)
 
-    }
+//     }
 
-    taskInput.value = "";
+//     taskInput.value = "";
 
-})
+// })
 
-taskGroup.addEventListener('click', function(e) {
-    if (e.target.tagName === 'LI') {
-        console.log(e.target)
-        e.target.classList.toggle('checked');
-    } else if (e.target.tagName === 'SPAN') {
-        e.target.parentElement.remove();
-    }
-}, false);
+// taskGroup.addEventListener('click', function(e) {
+//     if (e.target.tagName === 'DIV') {
+//         console.log(e.target)
+//         e.target.classList.toggle('checked');
+//     } else if (e.target.tagName === 'asd') {
+//         e.target.parentElement.remove();
+//     }
+// }, false);
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const taskGroup = document.querySelector('.list-group');
+
+    taskGroup.addEventListener('click', function(e) {
+        const target = e.target;
+        if (target.tagName === 'DIV' && target.classList.contains('list-group-item')) {
+            const rect = target.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            
+            if (x <= 20) {
+                target.classList.toggle('checked');
+            }
+        }
+    }, false);
+});
+
+
+//
 const headerBtn = document.querySelector('.header__menu');
 const aside = document.querySelector('.aside');
 
@@ -63,18 +81,20 @@ aside.addEventListener('mouseout', () => {
 });
 
 
-//
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('container');
-
     const firstDiv = document.querySelector('.list-group-item');
+
     firstDiv.addEventListener('keydown', handleKeydown);
 
     function handleKeydown(event) {
         if (event.key === 'Enter') {
             event.preventDefault(); 
             createNewEditableDiv(this);
+        } else if (event.key === 'Backspace' && this.textContent === '') {
+            event.preventDefault(); 
+            deleteCurrentDiv(this);
         }
     }
 
@@ -87,5 +107,28 @@ document.addEventListener('DOMContentLoaded', () => {
         container.insertBefore(newDiv, currentDiv.nextSibling);
         newDiv.focus();
     }
+
+    function deleteCurrentDiv(currentDiv) {
+        const previousDiv = currentDiv.previousElementSibling;
+
+        if (previousDiv) {
+            currentDiv.remove();
+            focusAtEnd(previousDiv);
+        } else {
+            currentDiv.textContent = ''; 
+        }
+    }
+
+    function focusAtEnd(element) {
+        const range = document.createRange();
+        const selection = window.getSelection();
+        range.selectNodeContents(element);
+        range.collapse(false); 
+        selection.removeAllRanges();
+        selection.addRange(range);
+        element.focus();
+    }
 });
+
+
 
