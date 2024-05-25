169b4714c2d803a81263d507c73af298561fd881
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-05-25 21:10:58 +0300

    commit

--- js/effeccts.js
+++ js/effeccts.js
@@ -1,15 +1,27 @@
 const searchTitle = document.querySelectorAll('.search__tool-block') 
-console.log(searchTitle)
 
 for (block of searchTitle) {
-    block.style.transition = '0.4s'
-    const blockElement = document.querySelector('.search__tool-block-element')
     block.addEventListener('mouseover', function () {
-        this.style.background = 'red'
-        blockElement.style.opacity = '0.7'
-    })
-    block.addEventListener('mouseout', function () {
-        this.style.background = 'none'
-        blockElement.style.opacity = '0'
+        for (block of searchTitle) {
+            block.classList.remove('active')
+        }
+        this.classList.add('active')
     })
 }
+
+// for (block of searchTitle) {
+//     block.style.transition = '0.4s'
+//     const blockElement = document.querySelector('.search__tool-block-element')
+//     block.addEventListener('mouseover', function () {
+//         this.style.background = 'rgba(255, 255, 255, 0.13)'
+//         this.classList.add('active')
+//         blockElement.style.opacity = '0.7'
+        
+//     })
+//     block.addEventListener('mouseout', function () {
+//         this.style.background = 'none'
+//         this.classList.remove('active')
+
+//         blockElement.style.opacity = '0'
+//     })
+// }
